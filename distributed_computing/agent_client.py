626596c8e74d14d5fd8c679444f1151840338fca
Milena Scholz <m.scholz97@web.de>
2019-07-08 13:55:23 +0200

    distributed computing

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,14 @@
 '''
 
 import weakref
+import threading
+import xmlrpclib
+import pickle
+import sys
+import os
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
+from keyframes import hello
+import numpy as np
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -17,50 +25,90 @@ class PostHandler(object):
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
         # YOUR CODE HERE
+        thread = threading.Thread(target = ClientAgent.execute_keyframes, args=(ClientAgent(), keyframes))
+        thread.start()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
         # YOUR CODE HERE
+        thread = threading.Thread(target = ClientAgent.set_transform, args=(ClientAgent(), effector_name, transform))
+        thread.start()
 
 
 class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
     '''
     # YOUR CODE HERE
+    
     def __init__(self):
         self.post = PostHandler(self)
+        self.proxy = xmlrpclib.ServerProxy('http://localhost:8000')
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        angle = self.proxy.get_angle(joint_name)
+        print angle
+        return angle
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        print self.proxy.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
-
+        posture = self.proxy.get_posture()
+        posture_deserialized = pickle.loads(posture)
+        print posture_deserialized
+        return posture_deserialized
+        
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        response = self.proxy.execute_keyframes(pickle.dumps(keyframes))
+        print response            
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        transform = self.proxy.get_transform(name)
+        transform_deserialized = pickle.loads(transform)
+        print transform_deserialized
+        return transform_deserialized
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        transform_serialized = pickle.dumps(transform)
+        response = self.proxy.set_transform(effector_name, transform_serialized)
+        print response
 
 if __name__ == '__main__':
     agent = ClientAgent()
     # TEST CODE HERE
-
+    print "setting LAnklePitch to 13.7"
+    agent.set_angle('LAnklePitch', 13.7)
+    print "get_angle returns:"
+    agent.get_angle('LAnklePitch')
+    print "get_posture returns:"
+    agent.get_posture()
+    print "executing keyframe hello"
+    agent.execute_keyframes(hello())
+    print "get_transform returns:"
+    agent.get_transform('LAnklePitch')
+    transform = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+    print "setting transform"
+    agent.set_transform('LAnklePitch', transform)
+    ph = PostHandler(agent)
+    print "executing keyframe hello (non-blocking)"
+    ph.execute_keyframes(hello())
+    print "setting transform (non-blocking)"
+    ph.set_transform('LAnklePitch', transform)
 
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -14,44 +14,67 @@
 # add PYTHONPATH
 import os
 import sys
+import pickle
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from recognize_posture import PostureRecognitionAgent
 from inverse_kinematics import InverseKinematicsAgent
 
 
-class ServerAgent(InverseKinematicsAgent):
+class ServerAgent(PostureRecognitionAgent, InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
     
+    
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.perception.joint[joint_name] = angle
+        return "set_angle complete"
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        recognized_posture = self.recognize_posture(self.perception)
+        return pickle.dumps(recognized_posture)
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = pickle.loads(keyframes)
+        return "execute_keyframes complete"
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        local_trans = self.local_trans(name, self.perception.joint[name])
+        return pickle.dumps(local_trans)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transforms(effector_name, pickle.loads(transform))
+        return "set_transform complete"
+
+        
+server = SimpleXMLRPCServer(("localhost", 8000))
+print "Listening on port 8000..."
+server.register_introspection_functions()
+server.register_instance(ServerAgent())
+server.serve_forever()
 
 if __name__ == '__main__':
     agent = ServerAgent()
